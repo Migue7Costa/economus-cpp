183d8376d7fe4bec45d7b9da2757715682790d03
Miguel Costa <miguelsscaugusto@gmail.com>
2024-04-29 17:02:22 -0300

    Adicionar arquivos de projeto.

---
+++ economus-cpp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using economus_cpp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace economus_cpp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ economus-cpp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace economus_cpp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
