4c5fa62a81f97d469b6b2781f5daf78465f5c134
Miguel Costa <miguelsscaugusto@gmail.com>
2024-05-04 17:02:49 -0300

    configuração do banco de dados e da tabela usuario

---
+++ economus-cpp/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace economus_cpp.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório inserir este campo")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório inserir este campo")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório inserir este campo")]
+        public string senha { get; set; }
+    }
+}
